5a52371d769424a1dfc1970c4400d8332c6efa64
Ginea Merrill <gineakanoe@gmail.com>
2021-09-20 22:51:51 -0400

    re-added div navBar to work better with js transition script

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    const headerHeight = 100;
+    const navBar = document.querySelector('.navBar');
+    const navBarHeight = 100;
 
     const distanceFromTop = Math.abs(
         document.body.getBoundingClientRect().top
-    );  if (distanceFromTop >= headerHeight) header.classList.add('fixed-top');
-        else header.classList.remove('fixed-top');
+    );  
+    
+    if (distanceFromTop >= navBarHeight) navBar.classList.add('fixed-top');
+    else navBar.classList.remove('fixed-top');
 });
